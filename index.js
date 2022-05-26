e15398491c630340f9b3e4a03fb1205f3edb81cf
j.perezu <j.perezurrutia@gmail.com>
2022-05-26 19:25:18 -0400

    API RESTfull almost fyll, pur method missing

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 const express = require('express');
+const { Router } = express;
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-const server = app.listen(PORT, () =>{
+const server = app.listen(PORT, () => {
   console.log(`Server http on ${PORT}...`);
 });
 server.on('error', error => console.log("Error on server", error));
 
-const Container = require('./desafioArchivos');
-
+const Container = require('./arrayMethods');
 const products = new Container('file.json');
+
+app.use('/static', express.static(__dirname + '/public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get("/", (request, resolve) => {
   resolve.send(
-    "<h1>Hola a todos</h1>"
+    "<h1>API RESTfull</h1>"
   )
 })
 
-app.get("/productos", async (request, resolve) => {
+const router = Router();
+
+router.get("/", async (request, resolve) => {
   try {
     const data = await products.getAll();
     resolve.send(data)
@@ -29,12 +35,47 @@ app.get("/productos", async (request, resolve) => {
   }
 })
 
-app.get("/productosRandom", async (request, resolve) => {
+router.get("/:id", async (request, resolve) => {
+
+  const id = Number(request.params.id)
+
   try {
-    const data = await products.randomItems();
-    resolve.send(data)
+    const data = await products.getByID(id);
+    if (data === undefined) {
+      resolve.send({ error: "product not found" })
+    } else {
+      resolve.send(data)
+    }
+
   } catch (e) {
     resolve.status(500);
     resolve.send(e)
   }
 })
+
+router.delete("/:id", async (request, resolve) => {
+
+  const id = Number(request.params.id)
+
+  try {
+    await products.deleteById(id);
+    resolve.send("Producto deleted");
+
+  } catch (e) {
+    resolve.status(500);
+    resolve.send(e)
+  }
+})
+
+router.post("/", (request, resolve) => {
+  const newData = request.body
+  products.save(newData);
+  resolve.send("Product saved")
+})
+
+router.put("/:id", (request, resolve) => {
+  const id = Number(request.params.id);
+
+})
+
+app.use("/api/products", router);
