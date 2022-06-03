f2a39a4635f6ea83b6f124af2fef0841ccb7a088
Javier Perez <j.perezurrutia@gmail.com>
2022-06-02 20:13:22 -0400

    test

--- API+EJS/index.js
+++ API+EJS/index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const Container = require('./arrayMethods');
-const products = new Container('file.json');
+const express = require("express");
+const Container = require("./arrayMethods");
+const products = new Container("file.json");
 
 const app = express();
 
@@ -9,26 +9,26 @@ const PORT = process.env.PORT || 8080;
 const server = app.listen(PORT, () => {
   console.log(`Server http on ${PORT}...`);
 });
-server.on('error', error => console.log("Error on server", error));
+server.on("error", (error) => console.log("Error on server", error));
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.set('views', './views')
-app.set('view engine', 'ejs')
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("views", "./views");
+app.set("view engine", "ejs");
 
 app.get("/", async (request, resolve) => {
   const data = await products.getAll();
-  resolve.render('index', {data})
-})
+  resolve.render("index", { data });
+});
 
 app.get("/products", async (request, resolve) => {
   const data = await products.getAll();
-  resolve.render('productsList',{data})
-})
+  resolve.render("productsList", { data });
+});
 
 app.post("/products", (request, resolve) => {
-  const newData = request.body
+  const newData = request.body;
   products.save(newData);
-  resolve.send("Product saved")
-})
+  resolve.send("Product saved");
+});
+
